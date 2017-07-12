41abe8d376583c6a2b0c9c0cc2f11b64fe355f75
Chanida K <fonsukoth@gmail.com>
2017-07-12 10:04:19 +0700

    using energia

---
+++ platform.h
@@ -0,0 +1,19 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include <Energia.h>
+#include <xdc/runtime/Error.h>
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/knl/Mailbox.h>
+#include <stdio.h>
+
+// State definition
+#define IDLE    0
+#define ID      1
+#define FUNC    2
+#define PARAM   3
+#define END     4
+
+#endif PLATFORM_H
+
